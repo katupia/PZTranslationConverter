fe574815b60d21614dd314a715363d07aa0891c2
Tchernobill <Katupia@hotmail.com>
2026-03-09 19:36:01 +0100

    Initial version

---
+++ PZTranslationConverter/Main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Convert;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        Console.WriteLine($"Arguments: {string.Join(", ", args)}");
+
+        if (args.Length == 0)
+        {
+            string[] args1 = { Environment.CurrentDirectory };//backup is executing directory (NOT the executable directory!)
+            return SubMain(args1);
+        }
+        else return SubMain(args);
+    }
+    static int SubMain(string[] args)
+    {
+        for (var i = 0; i < args.Length; i++)
+        {
+            if (File.Exists(args[i]))
+            {
+                applyToFile(args[i]);
+            }
+            else if (Directory.Exists(args[i]))
+            {
+                foreach (string filePath in getFiles(args[i]))
+                {
+                    applyToFile(filePath);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"ERROR: invalid param {i}: {args[i]}");
+            }
+        }
+        //System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+        return 0;
+    }
+    static void applyToFile(string filePath)
+    {
+        DetectedFileType fileType = Convert.TypeFileDetector.GetFileFormat(filePath);
+        switch (fileType) //apply algo according to file type
+        {
+            case DetectedFileType.txt:
+                {
+                    Console.WriteLine("TXT: " + filePath);
+                    Convert.Ctxt2json.translate(filePath);
+                } break;
+            default: Console.WriteLine("Not handled file extension "+ fileType + ": " + filePath); break;
+        }
+    }
+    static IEnumerable<string> getFiles(string directoryPath)
+    {
+        string[] files = Directory.GetFiles(directoryPath);//TODO TryCatch
+        for (int i = 0; i < files.Length; i++)
+        {
+            yield return files[i];
+        }
+        foreach (string subDir in Directory.GetDirectories(directoryPath))//TODO TryCatch
+        {
+            foreach (string filePath in getFiles(subDir))
+            {
+                yield return filePath;
+            }
+        }
+
+        yield break;
+    }
+}
---
+++ PZTranslationConverter/txt2json.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Convert
+{
+    internal class Ctxt2json
+    {
+        private static string[] languages = {
+            "_AR",
+            "_CA",
+            "_CH",
+            "_CN",
+            "_CS",
+            "_DA",
+            "_DE",
+            "_EN",
+            "_ES",
+            "_FI",
+            "_FR",
+            "_HU",
+            "_ID",
+            "_IT",
+            "_JP",
+            "_KO",
+            "_NL",
+            "_NO",
+            "_PH",
+            "_PL",
+            "_PT",
+            "_PTBR",
+            "_RO",
+            "_RU",
+            "_TH",
+            "_TR",
+            "_UA"
+        };
+        public static string convertFileName(string path)
+        {
+            //& remove name language suffix (e.g. _EN)
+            //& change file extension from .txt to .json
+            string dir = Path.GetDirectoryName(path);
+            string filename = Path.GetFileNameWithoutExtension(path);
+            foreach (string lang in languages)
+            {
+                filename = Regex.Replace(filename, lang, "");
+            }
+            filename += ".json";
+            return Path.Combine(dir, filename);
+        }
+
+        public static void translate(string path)
+        {
+            //write the json
+            //& remove name language suffix (remove _EN)
+            //& change file extension from .txt to .json
+            //& change format to utf-8 (without BOM)
+            //& replace
+            //key_. Name = "content_%-09*$^",
+            //with
+            //"key_. Name": "content_%-09*$^",
+            //& remove last,
+            //& remove anything before first {
+            //& replace special character codes ?
+            // Open the stream and read it back.
+            using StreamReader sr = File.OpenText(path);
+            string patternLine = "\\s*(\\S[^=^\\s]*)\\s*=\\s*(\"[^\"]*\")";
+            string outText = "{";
+            bool firstDone = false;
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                MatchCollection matches = Regex.Matches(s, patternLine);
+                foreach (Match match in matches)
+                {
+                    if (firstDone)
+                    {
+                        outText += ",";
+                    }
+                    string key = match.Groups[1].Value;
+                    string value = match.Groups[2].Value;
+                    outText += "\n    \"" + key + "\": " + value;
+                    firstDone = true;
+                }
+            }
+            outText += "\n}";
+
+            if (firstDone)
+            {
+                string outputPath = convertFileName(path);
+                using FileStream jsonFile = File.Create(outputPath);
+                UTF8Encoding encoding = new(false);//no BOM for PZ utf-8 of choice
+                byte[] outBytes = encoding.GetBytes(outText);
+                jsonFile.Write(outBytes, 0, outBytes.Length);
+            }
+        }
+    }
+}
